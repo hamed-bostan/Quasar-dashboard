a3833797104630342b719d13e1bfec0ddbe8916b
Hamed <hamed.bostan2@gmail.com>
2023-12-24 13:51:22 +0330

    daily activity style has finished

--- src/components/DailyActivity.vue
+++ src/components/DailyActivity.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="row">
-    <q-item v-for="task in tasks" :key="task.id" class="column bg-grey">
-      <q-item-lable>{{ task.title }}</q-item-lable>
-      <q-item-lable>{{ task.description }}</q-item-lable>
-      <q-img
+  <div class="daily-activity-main-container row">
+    <q-item v-for="task in tasks" :key="task.id" class="daily-activity column">
+    <q-img
       src='../assets/images/Doctor.png'
       spinner-color="white"
       style="height: 5rem; max-width: 5rem"
     />
-      <q-btn no-caps outline color="white" text-color="white" label="Resume" />
+    <div class="daily-activity-text">
+      <q-item-lable>{{ task.title }}</q-item-lable>
+      <q-item-lable>{{ task.description }}</q-item-lable>
+    </div>
+      <q-btn no-caps outline color="white" text-color="white" label="Resume" class="daily-activity-btn"/>
     </q-item>
   </div>
 </template>
@@ -39,4 +41,31 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+
+.daily-activity-main-container{
+  display: flex;
+  column-gap: 1rem;
+}
+
+.daily-activity{
+  width: 14.8125rem;
+  height: 16rem;
+  justify-content: center;
+  align-items: center;
+  background: #A5A5A5;
+  display: flex;
+  row-gap: 1.5rem;
+}
+
+.daily-activity-text{
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+}
+
+.daily-activity-btn{
+  width: 12.5rem;
+  height: 2rem;
+}
+</style>
--- src/components/PackageInventory.vue
+++ src/components/PackageInventory.vue
@@ -1,44 +1,65 @@
 <template>
-  <div class="row">
-    <q-item class="bg-grey column">
-      <q-item-lable>Day</q-item-lable>
-      <q-img
-      src='../assets/images/Ellipse.png'
-      spinner-color="white"
-      style="height: 2.5rem; max-width: 2.5rem"
-    />
-      <q-item-lable>inventory1</q-item-lable>
-    </q-item>
-    <q-item class="bg-grey column">
-      <q-item-lable>Conferance</q-item-lable>
+
+ <div class="q-pa-md example-row-all-breakpoints" >
+
+   <div class="row">
+        <div class="col">
+            <q-item>
+              Day
+            </q-item>
             <q-img
-      src='../assets/images/Ellipse.png'
-      spinner-color="white"
-      style="height: 2.5rem; max-width: 2.5rem"
-    />
-      <q-item-lable>inventory2</q-item-lable>
-    </q-item>
-  </div>
-  <div class="row">
-    <q-item class="bg-grey column">
-      <q-item-lable>Exercise</q-item-lable>
+            src='../assets/images/Ellipse.png'
+            spinner-color="white"
+            style="height: 2.5rem; max-width: 2.5rem"
+          />
+          <q-item-lable>inventory1</q-item-lable>
+        </div>
+
+          <div class="col">
+            <q-item>
+              Conferance
+            </q-item>
             <q-img
-      src='../assets/images/Ellipse.png'
-      spinner-color="white"
-      style="height: 2.5rem; max-width: 2.5rem"
-    />
-      <q-item-lable>inventory3</q-item-lable>
-    </q-item>
-    <q-item class="bg-grey column">
-      <q-item-lable>Online class</q-item-lable>
+            src='../assets/images/Ellipse.png'
+            spinner-color="white"
+            style="height: 2.5rem; max-width: 2.5rem"
+          />
+          <q-item-lable>inventory1</q-item-lable>
+        </div>
+      </div>
+
+    <div class="row">
+        <div class="col">
+            <q-item>
+              Exercise
+            </q-item>
             <q-img
-      src='../assets/images/Ellipse.png'
-      spinner-color="white"
-      style="height: 2.5rem; max-width: 2.5rem"
-    />
-      <q-item-lable>inventory4</q-item-lable>
-    </q-item>
-  </div>
+            src='../assets/images/Ellipse.png'
+            spinner-color="white"
+            style="height: 2.5rem; max-width: 2.5rem"
+          />
+          <q-item-lable>inventory1</q-item-lable>
+        </div>
+
+          <div class="col">
+            <q-item>
+              Online class
+            </q-item>
+            <q-img
+            src='../assets/images/Ellipse.png'
+            spinner-color="white"
+            style="height: 2.5rem; max-width: 2.5rem"
+          />
+          <q-item-lable>inventory1</q-item-lable>
+        </div>
+      </div>
+
+ </div>
+
+
+ 
+
+
 </template>
 <script>
 export default {
